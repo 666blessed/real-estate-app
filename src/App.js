89bab9c74f4d49a4af140231ee6fc46edc4b93d5
Katkov Kirill <katkov.kirill10@gmail.com>
2022-01-22 18:23:23 +0200

    added react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Explore from './pages/Explore';
+import ForgotPassword from './pages/ForgotPassword';
+import Offers from './pages/Offers';
+import Profile from './pages/Profile';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+
+
 function App() {
   return (
-    <div>
-      <h1>test</h1>
-      
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Explore/>}/>
+          <Route path='/offers' element={<Offers/>}/>
+          <Route path='/profile' element={<SignIn/>}/>
+          <Route path='/sign-in' element={<SignIn/>}/>
+          <Route path='/sign-up' element={<SignUp/>}/>
+          <Route path='/forgot-password' element={<ForgotPassword/>}/>
+          
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ src/pages/Offers.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Offers() {
+  return <h1>
+        Offer page
+    </h1>;
+}
+
+export default Offers;
