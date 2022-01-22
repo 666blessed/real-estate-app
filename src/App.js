cb81d5b7b8cfc54cf2099f6f6f0bbf80df8d1cb3
Katkov Kirill <katkov.kirill10@gmail.com>
2022-01-22 18:59:40 +0200

    navbar added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Offers from './pages/Offers';
 import Profile from './pages/Profile';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
+import Navbar from './components/Navbar';
 
 
 function App() {
@@ -17,9 +18,9 @@ function App() {
           <Route path='/profile' element={<SignIn/>}/>
           <Route path='/sign-in' element={<SignIn/>}/>
           <Route path='/sign-up' element={<SignUp/>}/>
-          <Route path='/forgot-password' element={<ForgotPassword/>}/>
-          
+          <Route path='/forgot-password' element={<ForgotPassword/>}/>      
         </Routes>
+        <Navbar/>
       </Router>
     </>
   );
