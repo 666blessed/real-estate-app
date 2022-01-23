459e56f7be498fd611e3b433ba516890068645c5
Katkov Kirill <katkov.kirill10@gmail.com>
2022-01-23 18:52:16 +0200

    profile upd

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import {getAuth} from 'firebase/auth';
+import {useEffect, useState} from 'react';
 
 function Profile() {
-  return <h1>Profile page</h1>;
+  const [user, setUser] = useState(null);
+  const auth = getAuth();
+  useEffect(() => {
+    setUser(auth.currentUser);
+  }, [])
+  return user ? <h1>{user.displayName}</h1> : 'Not logged in'
 }
 
 export default Profile;
