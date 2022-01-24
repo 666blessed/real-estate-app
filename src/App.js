8b17ee5ed4e7543ac1831a0393be244c5279200a
Katkov Kirill <katkov.kirill10@gmail.com>
2022-01-24 18:23:48 +0200

    CreateListing component added

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import PrivateRoute from './components/PrivateRoute';
 import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Category from './pages/Category';
-
+import CreateListing from './pages/CreateListing';
 
 function App() {
   return (
@@ -25,7 +25,8 @@ function App() {
           </Route>
           <Route path='/sign-in' element={<SignIn/>}/>
           <Route path='/sign-up' element={<SignUp/>}/>
-          <Route path='/forgot-password' element={<ForgotPassword/>}/>      
+          <Route path='/forgot-password' element={<ForgotPassword/>}/>    
+          <Route path='/create-listing' element={<CreateListing/>}/>    
         </Routes>
         <Navbar/>
       </Router>
